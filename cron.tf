c68809e72f50894caf200c7a9c126d131fa218cb
Ajish Antony <48723128+ajish-antony@users.noreply.github.com>
2021-10-06 02:29:37 +0530

    Update cron.tf

--- cron.tf
+++ cron.tf
@@ -1,10 +1,10 @@
-resource "aws_autoscaling_schedule" "scale_down" {
+  resource "aws_autoscaling_schedule" "scale_down" {
   scheduled_action_name = "scale_down"
   min_size = 1
   max_size = 1
   recurrence = "0 10 * * *"
   desired_capacity = 1
-  autoscaling_group_name = "${aws_autoscaling_group.asg1}"
+  autoscaling_group_name = "${aws_autoscaling_group.asg1.name}"
 }
 resource "aws_autoscaling_schedule" "scale_up" {
   scheduled_action_name = "scale_up"
@@ -12,5 +12,5 @@ resource "aws_autoscaling_schedule" "scale_up" {
   max_size = 2
   recurrence = "10 10 * * *"
   desired_capacity = 2
-  autoscaling_group_name = "${aws_autoscaling_group.asg1}"
+  autoscaling_group_name = "${aws_autoscaling_group.asg1.name}"
 }
